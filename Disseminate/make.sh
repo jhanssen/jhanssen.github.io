1869d78c7b3494c9d70011b949d0b76024718777
Jan Erik Hanssen <jhanssen@gmail.com>
2016-07-27 17:02:40 -0700

    Update

--- Disseminate/make.sh
+++ Disseminate/make.sh
@@ -6,6 +6,11 @@ if [[ -z $APPDMG ]]; then
    exit 1
 fi
 
+if [[ ! -f ~/dev/qt/5.7/clang_64/bin/macdeployqt ]]; then
+    echo "No ~/dev/qt/5.7/clang_64/bin/macdeployqt"
+    exit 1
+fi
+
 if [[ ! -f ~/dev/Disseminate/icons/AppIcon.icns ]]; then
    echo "No ~/dev/Disseminate/icons/AppIcon.icns"
    exit 1
@@ -16,8 +21,13 @@ if [[ ! -d  ~/dev/Disseminate/Disseminate.app ]]; then
     exit 1
 fi
 
+pushd ~/dev/Disseminate/
+~/dev/qt/5.7/clang_64/bin/macdeployqt Disseminate.app
+popd
+
 cp ~/dev/Disseminate/icons/AppIcon.icns Disseminate.icns
 rm -rf Disseminate.app
+rm Disseminate.dmg
 cp -Rv ~/dev/Disseminate/Disseminate.app .
 
 $APPDMG ./Disseminate.json ./Disseminate.dmg
